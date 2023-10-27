6969552ba58697c5a2b7c6fb261732a9a4dd306e
kaybao062 <116699898+kaybao062@users.noreply.github.com>
2023-10-27 21:14:36 +0000

    ew

--- app.rb
+++ app.rb
@@ -2,15 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  redirect("/add")
+  erb(:homepage)
 end
 
-get("/ad") do
-  erb(:add_form)
+get("/add") do
+  erb(:homepage)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
@@ -23,15 +24,20 @@ end
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
+  @result = @first_num - @second_num
 
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num * @second_num
+
   erb(:mult_result)
 end
 
@@ -40,5 +46,9 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_num / @second_num
+
   erb(:div_result)
 end
